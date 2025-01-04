795851b5148403f1da2e6ebdee9e53fc78bd4ebc
Saúl Rodríguez <saul.rodriguez@genstudents.org>
2025-01-04 14:23:33 -0300

    Trabajo Terminado

--- accion.js
+++ accion.js
@@ -1,31 +1,26 @@
-
-
-let form = document.getElementById('cta-form');
+let form = document.createElement('cta-form');
 
 form.addEventListener('click', function() {
-  let formElement = document.getElementById('form');
-formElement.scrollIntoView({ behavior: 'smooth' });
+    let formElement = document.getElementById("form");
+formElement.scrollIntoView({ behavior:'smooth' });
 });
 
-  function presionarBoton() {
-    // capturar los valores de los inputs
-    let nombre = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let mensaje = document.getElementById('message').value;
-    // enviar los valores en un objeto a la consola
-    
+    function presionarBoton() {
+        // capturar los valores de los inputs
+        let nombre = document.getElementById('name').value;
+        let email = document.getElementById('email').value;
+        let mensaje = document.getElementById('message').value;
+        // enviar los valores en un objeto a la consola
+        
+        if(nombre === '' || email === '' || mensaje === '') {
+            alert('Por favor, complete todos los campos');
+            return;
+        }
 
-    if(nombre === '' || email === '' || mensaje === '') {
-      alert('Por favor, complete todos los campos');
-      return;
+        alert('Gracias por tu ingreso, revisa la bandeja de entrada de tu correo');
+        console.log({nombre, email, mensaje});
+
+        document.getElementById('name').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('message').value = '';
     }
-
-    // mostrar un mensaje de alerta
-    alert('Gracias por enviar tu mensaje, revisa la consola por favor');
-    console.log({nombre, email, mensaje});
-    // limpiar los inputs
-    document.getElementById('name').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('message').value = '';
-  }
-  
